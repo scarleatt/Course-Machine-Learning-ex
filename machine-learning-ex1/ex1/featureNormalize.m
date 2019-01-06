75cae496e3b04dd3fc794937274c39874d58f934
resccu <fawnliu2333@gmail.com>
2019-01-06 10:20:38 +0800

    add ex2 directory

--- machine-learning-ex1/ex1/featureNormalize.m
+++ machine-learning-ex1/ex1/featureNormalize.m
@@ -26,9 +26,12 @@ sigma = zeros(1, size(X, 2));
 % Hint: You might find the 'mean' and 'std' functions useful.
 %       
 
-
-
-
+mu = mean(X);
+sigma = std(X);
+n = size(X,2);
+for i=1:n
+    X_norm(:,i) = (X_norm(:,i)-mu)/sigma;
+end
 
 
 
